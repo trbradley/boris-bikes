234906e64102ec6912285afa53cb54a9c94a161c
Chris Wynne <chris.wynndow@gmail.com>
2015-11-24 17:33:31 +0000

    Adds 11+12. Using instance variables and raising exceptions.

--- lib/DockingStation.rb
+++ lib/DockingStation.rb
@@ -1,7 +1,18 @@
-require File.dirname(__FILE__) + '/Bike.rb'
+require_relative '../lib/Bike.rb'
 
 class DockingStation
+
+  attr_accessor :docked
+
   def release_bike
+    if @docked == ''
+      raise 'No bikes available!'
+    end
     Bike.new
   end
+
+  def dock(bike)
+    @docked = bike
+  end
+
 end
--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -1,9 +1,22 @@
 require 'DockingStation'
-require 'Bike'
+
 describe DockingStation do
     it { is_expected.to respond_to :release_bike }
 
     it 'gets a bike and expects to be working' do
-    expect(DockingStation.new.release_bike.working?).to eq true
-  end
+      bike = subject.release_bike
+      expect(bike).to be_working
+    end
+
+    it 'allows a bike to be docked' do
+      bike = subject.release_bike
+      subject.dock(bike)
+      expect(subject.docked).to eq bike
+    end
+
+    it 'raises exception when no bikes' do
+      subject.docked = ''
+      expect {subject.release_bike}.to raise_error
+    end
+
 end
