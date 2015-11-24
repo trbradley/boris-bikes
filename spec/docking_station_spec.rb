bb15c2c4410a397570cf8db40f3a023622e0acd0
Chris Wynne <chris.wynndow@gmail.com>
2015-11-24 11:47:05 +0000

    Adds test to spec and method to dockingstation class

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -1,4 +1,10 @@
 require 'DockingStation'
 
 describe DockingStation do
+  it ' responds to the method release_bike' do
+    expect((DockingStation.new).respond_to?(:release_bike)).to eq true
+  end
+
+
+
 end
