3b3c0af5b89a02791471bb1b98392960e580f49e
emmabeynon <emma.beynon@gmail.com>
2015-11-25 16:00:50 +0000

    Adapted to have a capacity of 20 bikes

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -3,20 +3,20 @@
 require_relative 'bike'
 
 class DockingStation
-  attr_reader :bike
+  attr_reader :bikes
 
   def initialize
-    @bike = nil
+    @bikes = []
   end
 
   def dock(bike)
-    raise "This station is full" if @bike
-    @bike = bike
+    raise "This station is full" if @bikes.length == 20
+    @bikes << bike
   end
 
   def release_bike
-    raise "No bike available" unless @bike
-    @bike
+    raise "No bike available" if @bikes == []
+    @bikes.pop
   end
 
 end
--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -18,9 +18,13 @@ describe DockingStation do
    describe '#dock(bike)' do
      it { is_expected.to respond_to(:dock).with(1).argument }
 
+    #  it "has capacity to store 20 bikes" do
+    #    expect{20.times{subject.dock Bike.new}}.not_to raise_error
+    #  end
+
      it 'raises an error when the station is full' do
        subject.dock(Bike.new)
-       expect{subject.dock(Bike.new)}.to raise_error("This station is full")
+       expect{20.times{subject.dock(Bike.new)}}.to raise_error("This station is full")
      end
    end
 
@@ -33,14 +37,14 @@ describe DockingStation do
 
      it "tells us whats in the docking station when it's empty" do
        station = DockingStation.new
-       expect(station.bike).to eq nil
+       expect(station.bikes).to eq []
      end
 
      it "tells us what's in the docking station when it's full" do
        station = DockingStation.new
        bike = Bike.new
        station.dock(bike)
-       expect(station.bike).to eq bike
+       expect(station.bikes).to eq [bike]
      end
     end
 
