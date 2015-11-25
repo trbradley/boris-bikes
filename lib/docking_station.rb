9509fc68f5f81345f257bd90a3e8d3355231dc24
emmabeynon <emma.beynon@gmail.com>
2015-11-25 11:16:22 +0000

    Defined dock and initialize methods and @bike instance variables

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -3,7 +3,17 @@
 require_relative 'bike'
 
 class DockingStation
+  attr_reader :bike
+
+  def initialize
+    @bike = nil
+  end
+
   def release_bike
     Bike.new
+  end
+
+  def dock(bike)
+    bike
   end
 end
