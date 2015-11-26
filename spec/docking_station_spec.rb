c4693c9b1166e4a11bc0ada36dec4f59fb5219fa
giamir <giamir.buoncristiani@gmail.com>
2015-11-26 17:50:33 +0000

    introduces doubles and mocks in unit tests

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -1,87 +1,75 @@
-require "docking_station"
+require 'docking_station'
 
 describe DockingStation do
-   it { is_expected.to respond_to :release_bike }
+  it { is_expected.to respond_to :release_bike }
 
-   it "creates a docking station with a default capacity" do
-     expect(subject.capacity).to eq DockingStation::DEFAULT_CAPACITY
-   end
+  describe '#initialize' do
+    it 'creates a docking station with a default capacity' do
+      expect(subject.capacity).to eq subject.class::DEFAULT_CAPACITY
+    end
 
-   it "allows to set a capacity of 50" do
-     station = DockingStation.new(50)
-     expect(station.capacity).to eq 50
-   end
+    it 'allows to set a capacity of 50' do
+      station = subject.class.new(50)
+      expect(station.capacity).to eq 50
+    end
+  end
 
-   describe '#release_bike' do
-     it 'releases a bike' do
-       bike = Bike.new
-       subject.dock(bike)
-       expect(subject.release_bike).to eq bike
-     end
-
-     it "gives us an error message when the station is empty" do
-       expect{subject.release_bike}.to raise_error("No bike available")
-     end
-
-     it "give us an error message when trying to release a broken bike" do
-      bike = Bike.new
-      bike.report
+  describe '#release_bike' do
+    it 'releases a bike' do
+      bike = double(:bike, working?: true)
       subject.dock(bike)
-      expect{subject.release_bike}.to raise_error("This bike is broken")
-     end
+      expect(subject.release_bike).to eq bike
+    end
 
-     it "keeps a broken bike in the dock" do
+    it 'gives us an error message when the station is empty' do
+      expect { subject.release_bike }.to raise_error('No bike available')
+    end
+
+    it 'give us an error message when trying to release a broken bike' do
+      bike = double(:bike, working?: false)
+      subject.dock(bike)
+      expect { subject.release_bike }.to raise_error('This bike is broken')
+    end
+
+    it 'keeps a broken bike in the dock' do
       begin
-        bike = Bike.new
+        bike = double(:bike, working?: false)
         subject.dock(bike)
-        bike.report
         subject.release_bike
       rescue Exception
         expect(subject.bikes).to eq [bike]
       end
-     end
+    end
 
-     it "gives me a bike if there is a working bike in the docking station" do
+    it 'gives me a bike if there is a working bike in the docking station' do
       4.times do
-        bike = Bike.new
-        bike.report
+        bike = double(:bike, working?: false)
         subject.dock(bike)
       end
-        goodbike = Bike.new
-        subject.dock(goodbike)
+      goodbike = double(:bike, working?: true)
+      subject.dock(goodbike)
       5.times do
-        bike = Bike.new
-        bike.report
+        bike = double(:bike, working?: false)
         subject.dock(bike)
       end
       expect(subject.release_bike).to eq goodbike
-     end
-   end
-
-   describe '#dock(bike)' do
-     it { is_expected.to respond_to(:dock).with(1).argument }
-
-     it 'raises an error when the station is full' do
-       subject.capacity.times{ subject.dock(Bike.new) }
-       expect{ subject.dock(Bike.new) }.to raise_error("This station is full")
-     end
-   end
-
-   describe '#bike' do
-     it 'gets a working bike' do
-       subject.dock(Bike.new)
-       expect(Bike.new).to be_working
-     end
-
-     it "tells us whats in the docking station when it's empty" do
-       expect(subject.bikes).to eq []
-     end
-
-     it "tells us what's in the docking station when it's full" do
-       bike = Bike.new
-       subject.dock(bike)
-       expect(subject.bikes).to eq [bike]
-     end
     end
+  end
 
+  describe '#dock(bike)' do
+    it { is_expected.to respond_to(:dock).with(1).argument }
+    it 'raises an error when the station is full' do
+      subject.capacity.times{ subject.dock(double(:bike)) }
+      expect { subject.dock(double(:bike)) }.to raise_error('This station is full')
+    end
+  end
+
+  describe '#bikes' do
+    it { is_expected.to respond_to(:bikes) }
+    it 'should return an array with the bikes in the docking station' do
+      array = []
+      2.times { bike = double(:bike); array << bike; subject.dock(bike) }
+      expect(subject.bikes).to match_array(array)
+    end
+  end
 end
