d112322084a47f0bc734cbbabe8fce0b38cdcb1f
Sara Tateno <sara.tateno@gmail.com>
2015-11-26 11:04:22 +0000

    Added report function to change bike working status

--- spec/bike_spec.rb
+++ spec/bike_spec.rb
@@ -4,4 +4,16 @@ describe Bike do
   it { is_expected.to respond_to :working? }
   it { is_expected.to respond_to :report}
 
+
+
+
+  describe "#working?" do
+
+    it "reports that a bike is not working" do
+      subject.report
+      expect(subject.working?).to be false
+    end
+
+  end
+
 end
