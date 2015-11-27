fb611426e1221fff5d704629b4deaf2e84e931af
giamir <giamir.buoncristiani@gmail.com>
2015-11-27 14:19:59 +0000

    Add instance variable bikes for van and implement load method

--- lib/van.rb
+++ lib/van.rb
@@ -1,4 +1,13 @@
 class Van
-  def load bike
+  def initialize
+    @bikes = []
   end
+
+  def load(bike)
+    bikes << bike
+  end
+
+  private
+
+  attr_reader :bikes
 end
--- spec/van_spec.rb
+++ spec/van_spec.rb
@@ -4,6 +4,11 @@ describe Van do
 
   describe '#load' do
     it { is_expected.to respond_to(:load).with(1).argument }
+
+    it 'loads bike on to van' do
+      bike = double(:bike)
+      expect(subject.load(bike)).to eq [bike]
+    end
   end
 
 end
