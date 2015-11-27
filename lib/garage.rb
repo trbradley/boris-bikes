e129d90108431717f580d8c8224f89a848228778
giamir <giamir.buoncristiani@gmail.com>
2015-11-27 16:33:51 +0000

    define and implement release_working_bike method for garage

--- lib/garage.rb
+++ lib/garage.rb
@@ -1,5 +1,4 @@
 class Garage
-
   def initialize
     @bikes = []
   end
@@ -8,7 +7,21 @@ class Garage
     bikes << bike
   end
 
+  def release_working_bike
+    fail 'No bikes in garage!' if empty?
+    fail 'No working bikes in garage!' if broken_only?
+    bikes.delete(bikes.find(&:working?))
+  end
+
   private
 
   attr_reader :bikes
+
+  def empty?
+    bikes.empty?
+  end
+
+  def broken_only?
+    !bikes.find(&:working?)
+  end
 end
